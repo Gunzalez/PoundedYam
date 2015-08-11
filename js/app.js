22b93df92a77c29d4c4bf6f9b16d2476f6482d59
Segun Konibire <gunzalez@hotmail.com>
2015-08-11 01:39:23 +0100

    First annoying commit

---
+++ js/app.js
@@ -0,0 +1,3 @@
+angular.module('PoundedYam', [
+    'PoundedYam.controllers'
+]);
---
+++ js/controllers.js
@@ -0,0 +1,33 @@
+angular.module('PoundedYam.controllers', [])
+
+    .controller('homeController', function($scope) {
+        $scope.hero = {
+            src: "hero.jpg",
+            alt: "Pounded Yam"
+        };
+        $scope.ads = [
+            { name: 'Shop 1', link: 'http://www.shop1.com', banner: '/img/ad/shop1.png' },
+            { name: 'Shop 2', link: 'http://www.shop2.com', banner: '/img/ad/shop2.png' }
+        ]
+    })
+
+    .controller('listController', function($scope) {
+        $scope.meals = [
+            {
+                id: "1",
+                name: "Pounded Yam"
+            },
+            {
+                id: "2",
+                name: "Beans"
+            },
+            {
+                id: "3",
+                name: "Plantain"
+            },
+            {
+                id: "4",
+                name: "Pepper Soup"
+            }
+        ];
+    });
