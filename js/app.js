45e620b577ac0c9b3e3c3f21c66dbf50e73c283a
Segun Konibire <gunzalez@hotmail.com>
2015-08-15 18:30:52 +0100

    Adds in simple routing

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
-angular.module('PoundedYam', [
-    'PoundedYam.controllers'
+var poundedYam = angular.module('PoundedYam', [
+    'PoundedYam.controllers',
+    'ngRoute'
 ]);
+
+poundedYam.config(['$routeProvider',
+    function($routeProvider) {$routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'homeController'
+        })
+        .when('/list', {
+            templateUrl: 'partials/list.html',
+            controller: 'listController'
+        })
+        .when('/cook/:recipeId', {
+            templateUrl: 'partials/detail.html',
+            controller: 'detailController'
+        })
+        .when('/shop/:recipeId', {
+            templateUrl: 'partials/shops.html',
+            controller: 'shopsController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
--- js/controllers.js
+++ js/controllers.js
@@ -6,32 +6,49 @@ angular.module('PoundedYam.controllers', [])
             alt: "Pounded Yam"
         };
         $scope.ads = [
-            { name: 'Shop 1', link: 'http://www.shop1.com', banner: '/img/ad/shop1.png' },
-            { name: 'Shop 2', link: 'http://www.shop2.com', banner: '/img/ad/shop2.png' }
+            { title: 'Shop 1', link: 'http://www.shop1.com', banner: '/img/ad/shop1.png' },
+            { title: 'Shop 2', link: 'http://www.shop2.com', banner: '/img/ad/shop2.png' }
         ]
     })
 
+    .controller('detailController', function($scope, $routeParams) {
+        $scope.id = $routeParams.recipeId;
+
+
+    })
+
+    .controller('shopsController', function($scope, $routeParams) {
+        $scope.id = $routeParams.recipeId;
+
+    })
+
     .controller('listController', function($scope) {
-        $scope.meals = [
+        $scope.recipes = [
             {
-                id: "1",
-                name: "Pounded Yam"
+                id: "12",
+                name: "Pounded Yam",
+                thumb: "/img/meals/meal-pounded-yam.png"
             },
             {
-                id: "2",
-                name: "Beans"
+                id: "22",
+                name: "Beans",
+                thumb: "/img/meals/meal-beans.png"
             },
             {
                 id: "3",
-                name: "Plantain"
+                name: "Plantain",
+                thumb: "/img/meals/meal-plantain.png"
             },
             {
                 id: "4",
-                name: "Pepper Soup"
+                name: "Pepper Soup",
+                thumb: "/img/meals/meal-pepper-soup.png"
             },
             {
-                id: "5",
-                name: "Okra Soup"
+                id: "55",
+                name: "Okra Soup",
+                thumb: "/img/meals/meal-okra-soup.png"
             }
         ];
     });
+
